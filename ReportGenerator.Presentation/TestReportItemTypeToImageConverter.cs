a02e1d6e0de7ea522501a55d5ba23168bc55daa1
Tom <Tom@DESKTOP-HOH30DU>
2020-05-16 07:41:08 +0100

    Move converter into presentation layer

--- ReportGenerator.Presentation/TestReportItemTypeToImageConverter.cs
+++ ReportGenerator.Presentation/TestReportItemTypeToImageConverter.cs
@@ -3,7 +3,7 @@ using System.Globalization;
 using System.Windows.Data;
 using Report.Components;
 
-namespace ReportGenerator.View
+namespace ReportGenerator.Presentation
 {
     [ValueConversion(typeof(ReportItemType),typeof(string))]
     public class TestReportItemTypeToImageConverter : IValueConverter
